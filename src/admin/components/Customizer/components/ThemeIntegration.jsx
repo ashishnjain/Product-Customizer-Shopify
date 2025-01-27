f2bdd73c9c367b889869665bbaa0c15655c742f2
Nidhi-NBL <nidhi.s@naapbooks.com>
2025-01-27 12:16:17 +0530

    s

--- src/admin/components/Customizer/components/ThemeIntegration.jsx
+++ src/admin/components/Customizer/components/ThemeIntegration.jsx
@@ -116,7 +116,7 @@ const ThemeIntegration = ({ onBack }) => {
 
               {appEmbed === 'deactivated' && (
                 <div className="alert alert-info">
-                  To display options on your Online Store, you must enable app embed in your themes.
+                  To display options on your Online Store, you must enable app embed in your theme.
                 </div>
               )}
 
