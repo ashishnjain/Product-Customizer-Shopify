cc3ef1fe59172093d15a80261ea239f4b5264cdc
Devarsh Soni <devarsh.s@naapbooks.com>
2025-02-04 12:19:42 +0530

    backup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminDashboard from './admin/pages/AdminDashboard';
 import ProductPage from './frontend/pages/products/ProductPage';
 import "./assets/admin/customizer.css";
@@ -14,12 +14,13 @@ import "./assets/frontend/customization.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <h1>Product Customizer</h1>
-        {/* Add your app components here */}
-      </div>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AdminDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/products/:id" element={<ProductPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,22 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/frontend/styles.css";
-import { AppProvider } from '@shopify/app-bridge-react';
 
 export { default as ProductCustomizer } from "./frontend/pages/products/FrontendPreviewModal";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const config = {
-  apiKey: process.env.REACT_APP_SHOPIFY_API_KEY,
-  host: new URL(window.location).searchParams.get("host"),
-  forceRedirect: true
-};
-
-root.render(
-  <React.StrictMode>
-    <AppProvider config={config}>
-      <App />
-    </AppProvider>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
